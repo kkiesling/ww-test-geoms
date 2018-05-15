5067d93ce445d4890945cb4eaf7219567a10e7df
Kalin Kiesling <krkiesling@gmail.com>
2018-05-15 14:29:56 -0500

    fixed segfault, new file system for separating files

--- test-isovols.py
+++ test-isovols.py
@@ -2,8 +2,8 @@ import vol as v
 
 def main():
     g = v.IsoVolumes('fng-ww-tags.vtk', 'ww_n')
-    #levels = [1.e-6, 8.706e-6, 7.579e-5, 6.598e-4, 5.743e-3, 0.05]
-    levels = [6.598e-4, 5.743e-3, 0.05]
+    levels = [1.e-6, 8.706e-6, 7.579e-5, 6.598e-4, 5.743e-3, 0.05]
+    #levels = [6.598e-4, 5.743e-3, 0.05]
     g.assign_levels(levels)
     g.create_geometry()
     #geom1.generate_volumes()
