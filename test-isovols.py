f069cd5570d3cfd35846a2eb85a80821c4180396
Kalin Kiesling <krkiesling@gmail.com>
2018-07-09 14:00:08 -0500

    new class name

--- test-isovols.py
+++ test-isovols.py
@@ -1,7 +1,7 @@
 import vol as v
 
 def main():
-    g = v.IsoVolumes('fng-ww-tags.vtk', 'ww_n')
+    g = v.IsoVolume('fng-ww-tags.vtk', 'ww_n')
     levels = [1.e-6, 8.706e-6, 7.579e-5, 6.598e-4, 5.743e-3, 0.05]
     #levels = [6.598e-4, 5.743e-3, 0.05]
     g.assign_levels(levels)
