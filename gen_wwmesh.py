90dc26d2ae47fce33dd30a81e845bed68874af5b
Kalin Kiesling <krkiesling@gmail.com>
2019-01-24 14:41:07 -0600

    better data for more levels

--- gen_wwmesh.py
+++ gen_wwmesh.py
@@ -11,7 +11,10 @@ m.t = NativeMeshTag(size=1, default=0.01, mesh=m, name='ww_val')
 
 for i in m.iter_ve():
     c = m.ve_center(i)
-    if (-5. < c[0] < 5.) and (-10. < c[1] < 10.) and (-5. < c[2] < 5.):
-        m.t[i] = 0.5
+    if (-8. < c[0] < 8.) and (-8. < c[1] < 8.) and (-8. < c[2] < 8.):
+        m.t[i] = .5
+    if (-4. < c[0] < 4.) and (-4. < c[1] < 4.) and (-4. < c[2] < 4.):
+        m.t[i] = 1.0
+
 
 m.write_hdf5('ww-mesh.h5m')
